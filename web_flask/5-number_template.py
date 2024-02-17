d2dc45b7efa38cd0531ae26e005ee087509bfe3a
mariam salama <mariamsalama888@gmail.com>
2024-02-17 14:26:50 -0800

    num

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def integer(n):
 @web_flask.route('/number_template/<int:n>', strict_slashes=False)
 def num(n=None):
     if isinstance(n, int):
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 
